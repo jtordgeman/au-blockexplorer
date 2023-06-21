3db205fd936f62f1c6e3381cb773d92b52b19ef3
buserian <buha124a@yahoo.com>
2023-06-21 09:22:44 +0300

    initial

---
+++ src/components/AppBar.js
@@ -0,0 +1,9 @@
+const AppBar = () => {
+    return (
+        <div className="w-full h-8 bg-green-300 flex justify-center items-center">
+            <h1 className="">Leet Block Explorer 🎉</h1>
+        </div>
+    );
+};
+
+export default AppBar;
